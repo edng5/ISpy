efe9c36269a51617c4c7354fe6d36dd51605b4f3
Edward Ng <66270109+edng5@users.noreply.github.com>
2022-05-17 13:55:08 -0400

    added object detection

--- ISpy.py
+++ ISpy.py
@@ -1,7 +1,7 @@
 # I Spy by Edward Ng
 # User picks an object and says its colour. The computer searches all objects of that colour in view
 # and guesses which object was chosen.
-# 5/11/2022
+# 5/17/2022
 
 import cv2
 import numpy as np
@@ -17,6 +17,20 @@ def map_colours(colour, frame):
 
 if __name__ == "__main__":
     cap = cv2.VideoCapture(0)
+
+    classNames = []
+    classFile = 'coco.names'
+    with open(classFile, 'rt') as f:
+        classNames = f.read().rstrip('\n').split('\n')
+
+    configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+    weightsPath = 'frozen_inference_graph_coco.pb'
+
+    net = cv2.dnn_DetectionModel(weightsPath, configPath)
+    net.setInputSize(320, 320)
+    net.setInputScale(1.0/127.5)
+    net.setInputMean((127.5, 127.5, 127.5))
+    net.setInputSwapRB(True)
 
     print("Press q to quit.")
 
@@ -36,7 +50,6 @@ if __name__ == "__main__":
 
             # find the colours specified by user
             mask = map_colours(colour, frame)
-
             #define kernel size  
             kernel = np.ones((7,7),np.uint8)
             # Remove unnecessary noise from mask
@@ -51,6 +64,13 @@ if __name__ == "__main__":
             output = cv2.drawContours(segmented_img, contours, -1, (0, 0, 255), 3)
 
             output = cv2.drawContours(frame, contours, -1, (0, 0, 255), 3)
+
+            classIds, confs, bbox = net.detect(frame, confThreshold=0.5)
+
+            if len(classIds) != 0:
+                for classId, conf, box in zip(classIds.flatten(), confs.flatten(), bbox):
+                    cv2.rectangle(output, box, color=(0,255,0), thickness=2)
+                    cv2.putText(output, classNames[classId - 1].upper(), (box[0] + 10, box[1] + 30), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
 
             # output camera with contour lines
             cv2.imshow("I Spy", output)
